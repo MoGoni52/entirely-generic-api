309146eded429ce0f4cb3bdb445e9811f745acf8
mohammedgoni <mohammedsigoni@gmail.com>
2022-07-18 15:28:42 +0100

    add fetch-request

---
+++ client/assets/scripts/index.js
@@ -0,0 +1,14 @@
+function loadRestaurantDetails() {
+
+    const url = "http://localhost:3000";
+
+    fetch(url)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+loadRestaurantDetails();
