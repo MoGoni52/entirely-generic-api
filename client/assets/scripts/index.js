56d2c16d645022cdf0c5bfc3e9a132cf87273e53
mohammedgoni <mohammedsigoni@gmail.com>
2022-07-18 16:02:16 +0100

    update api

--- client/assets/scripts/index.js
+++ client/assets/scripts/index.js
@@ -1,11 +1,21 @@
+function displayRestaurantDetails(data) {
+    // Get a reference to the heading
+    const heading = document.getElementById("restaurant-name");
+    heading.textContent = data["name"];
+
+    document.getElementById("established-year");
+    heading.textContent = data["established"];
+
+    document.getElementById("restaurant-status").textContent = data["status"];
+}
+
 function loadRestaurantDetails() {
 
     const url = "http://localhost:3000";
 
     fetch(url)
-        .then(res => {
-            console.log(res);
-        })
+        .then(res => res.json())
+        .then(data => displayRestaurantDetails(data))
         .catch(err => {
             console.log(err);
         })
